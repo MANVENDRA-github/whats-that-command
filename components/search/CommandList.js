2df30648e4ef4437a63c153c8151a202a7d9db1d
Manvendra <mksingh5904@gmail.com>
2026-06-12 23:49:43 +0530

    feat(search): stagger result cards like scrolling output

--- components/search/CommandList.js
+++ components/search/CommandList.js
@@ -3,9 +3,10 @@ import CommandCard from './CommandCard';
 export default function CommandList({ commands }) {
   return (
     <ul className="space-y-3">
-      {commands.map((cmd) => (
+      {commands.map((cmd, i) => (
         <li key={cmd.id}>
-          <CommandCard cmd={cmd} />
+          {/* stagger the first few results like terminal output scrolling in */}
+          <CommandCard cmd={cmd} style={{ animationDelay: `${Math.min(i, 8) * 45}ms` }} />
         </li>
       ))}
     </ul>
